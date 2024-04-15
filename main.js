976a4acbc294986354f1128b9e4060e701094512
shayan509 <si757289@gmail.com>
2024-04-16 01:41:29 +0500

    this is a commit

---
+++ main.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+console.log("Answer The below questions , note that the rates in our program are not ofter updated.");
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GPB: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let userAns = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from currency.",
+        type: "list",
+        choices: ["USD", "EUR", "GPB", "INR", "PKR"],
+    },
+    {
+        name: "to",
+        message: "Enter to currency.",
+        type: "list",
+        choices: ["USD", "EUR", "GPB", "INR", "PKR"],
+    },
+    {
+        name: "amount",
+        message: "Enter your amount.",
+        type: "number",
+    },
+]);
+let fromAmount = currency[userAns.from];
+let toAmount = currency[userAns.to];
+let amount = userAns.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(`Your amount is ${convertedAmount}.`);
+console.log(`Thanks for using our program.`);
